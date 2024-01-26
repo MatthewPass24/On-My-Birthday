28226331edbe834ecfc263b0045672bd1d2a61dd
MatthewPass24 <matthewpass24@gmail.com>
2024-01-26 08:56:44 -0500

    started javascript to change text on click

---
+++ script.js
@@ -0,0 +1,30 @@
+    function originalYear(){
+        document.getElementById('diffImage').src = 'images/baby.png'
+        document.getElementById('change-paragraph').innerText='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Fermentum dui faucibus in ornare quam viverra orci. Mauris pellentesque pulvinar pellentesque habitant morbi tristique.'
+        document.getElementById('changeDate').innerText= 'January 24, 2007'
+    }
+    function year1935(){
+        document.getElementById('diffImage').src = 'images/hockey.png'
+        document.getElementById('change-paragraph').innerText='On this day, the first canned beer was sold'
+        document.getElementById('changeDate').innerText= 'January 24, 1935'
+    }
+    function year1970(){
+        document.getElementById('diffImage').src = 'images/dalycity.png'
+        document.getElementById('change-paragraph').innerText='The wonderful Matthew Lillard graced the world with his presence'
+        document.getElementById('changeDate').innerText= 'January 24, 1970'
+    }
+    function year1989(){
+        document.getElementById('diffImage').src = 'images/beatle.png'
+        document.getElementById('change-paragraph').innerText='This day marked the end of a disgusting figure. Ted Bundy was executed finally.'
+        document.getElementById('changeDate').innerText= 'January 24, 1989'
+    }
+    function year1995(){
+        document.getElementById('diffImage').src = 'images/world.png'
+        document.getElementById('change-paragraph').innerText='OJ Simpson Started'
+        document.getElementById('changeDate').innerText= 'January 24, 1995'
+    }
+    function year2013(){
+        document.getElementById('diffImage').src = 'images/ice.png'
+        document.getElementById('change-paragraph').innerText='The US ends ban on women in combat'
+        document.getElementById('changeDate').innerText= 'January 24, 2013'
+    }

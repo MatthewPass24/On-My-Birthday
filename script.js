979481933bcffa9bdd6bd4770d6c020911445fbf
MatthewPass24 <matthewpass24@gmail.com>
2024-01-30 22:17:24 -0500

    added all images and finished descriptions

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
     function originalYear(){
-        document.getElementById('diffImage').src = 'images/baby.png'
+        document.getElementById('diffImage').src = 'images/me.jpg'
         document.getElementById('change-paragraph').innerText='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Fermentum dui faucibus in ornare quam viverra orci. Mauris pellentesque pulvinar pellentesque habitant morbi tristique.'
         document.getElementById('changeDate').innerText= 'January 24, 2007'
     }
     function year1935(){
         document.getElementById('diffImage').src = 'images/beer.png'
-        document.getElementById('change-paragraph').innerText='On this day, the first canned beer was sold'
+        document.getElementById('change-paragraph').innerText="The official birthday of canned beer was on January 24, 1935. This day marked the debut of the cans Krueger's Finest Beer and Krueger's Cream Ale."
         document.getElementById('changeDate').innerText= 'January 24, 1935'
     }
     function year1970(){
-        document.getElementById('diffImage').src = 'images/dalycity.png'
-        document.getElementById('change-paragraph').innerText='The wonderful Matthew Lillard graced the world with his presence'
+        document.getElementById('diffImage').src = 'images/lillard.webp'
+        document.getElementById('change-paragraph').innerText="Zoinks! What a day! Actor Matthew Lillard was born on January 24, 1970. Lillard is best known for starring as Stu in Scream (1996), Shaggy in Scooby Doo (2000), and recently as William Afton in Five Nights at Freddy's (2023)."
         document.getElementById('changeDate').innerText= 'January 24, 1970'
     }
     function year1989(){
         document.getElementById('diffImage').src = 'images/bundy.webp'
-        document.getElementById('change-paragraph').innerText='This day marked the end of a disgusting figure. Ted Bundy was executed finally.'
+        document.getElementById('change-paragraph').innerText='Ted Bundy was finally sentenced to death via electrocution on January 24, 1989. He was a serial killer who murdered dozens of young women and girls in the 1970s.'
         document.getElementById('changeDate').innerText= 'January 24, 1989'
     }
     function year1995(){
-        document.getElementById('diffImage').src = 'images/world.png'
-        document.getElementById('change-paragraph').innerText='The OJ Simpson Trial Began'
+        document.getElementById('diffImage').src = 'images/OJ.webp'
+        document.getElementById('change-paragraph').innerText='In the O.J. Simpson trial, Simpson (former professional football player) was tried of the murder of his ex-wife (Nicole Brown Simpson) and her friend. The case started on January 24, and even though there was evidence, Simpson was proven not guilty and acquitted later that year.'
         document.getElementById('changeDate').innerText= 'January 24, 1995'
     }
     function year2013(){
-        document.getElementById('diffImage').src = 'images/ice.png'
-        document.getElementById('change-paragraph').innerText='The US ends ban on women in combat'
+        document.getElementById('diffImage').src = 'images/women.jpg'
+        document.getElementById('change-paragraph').innerText='A momentous change in the military occured on this day. The Pentagon ended the ban on women in frontline combat, opening up hundreds of thousands of positions that were previously held by men.'
         document.getElementById('changeDate').innerText= 'January 24, 2013'
     }
